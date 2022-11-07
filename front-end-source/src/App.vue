f262d982b4806cc985f54676f0792034cd6f4f5d
Jove <2070721350@qq.com>
2022-11-07 15:50:41 +0800

    feat:前端项目初始化

--- front-end-source/src/App.vue
+++ front-end-source/src/App.vue
@@ -1,8 +1,4 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
   <div>
@@ -15,6 +11,10 @@ import HelloWorld from './components/HelloWorld.vue'
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
 
 <style scoped>
 .logo {
--- todo.md
+++ todo.md
@@ -1,7 +1,15 @@
 # TODO
 
+-   [ ] ### 开启 ESlint
+
 -   [ ] ### 核心 point 页面设计
 
 -   [ ] ### 引入核心手势识别库
+
+    https://victordibia.com/handtrack.js/#/
+
+    https://www.npmjs.com/package/handtrackjs
+
+    https://github.com/victordibia/handtrack.js
 
 尝试通过位置控制鼠标
