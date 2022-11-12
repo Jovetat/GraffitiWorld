a531e0a2f2b5598ecf7aa7ea6c1f61933fb2a274
Jove <2070721350@qq.com>
2022-11-12 18:00:40 +0800

    feat:配置Antd按需注入

--- front-end-source/vite.config.js
+++ front-end-source/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import { resolve } from 'path'
+// Antd 组件按需注入
+import ViteComponents from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   plugins: [
@@ -9,6 +12,34 @@ export default defineConfig({
     eslintPlugin({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue'],
     }),
+    {
+      name: 'vite-plugin-antd',
+      apply: 'serve',
+      transform: (code, id) => {
+        if (/src\/main.js$/.test(id)) {
+          const finalCode =
+            `import Antd from 'ant-design-vue';import 'ant-design-vue/dist/antd.less';${code}`.replace(
+              'createApp(App)',
+              'createApp(App).use(Antd)',
+            )
+          return {
+            code: finalCode,
+            map: null,
+          }
+        }
+      },
+    },
+    process.env.NODE_ENV === 'production'
+      ? ViteComponents({
+          resolvers: [
+            AntDesignVueResolver({
+              importStyle: 'less',
+              // resolveIcons: true,
+            }),
+          ],
+          include: [/\.vue$/, /\.jsx?$/],
+        })
+      : {},
   ],
   resolve: {
     alias: {
